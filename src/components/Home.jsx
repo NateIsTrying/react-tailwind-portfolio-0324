ee7b541159d30d1717ae6d002e6f3c18507d8007
Nathan Kim <novicenate@pop-os.localdomain>
2024-03-26 22:17:17 -0400

    prepping for Background Canvas, created components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,14 @@ function Home() {
 
     return (
         <>
-          <h1 class="intro" >Hello, I'm <span class="highlight">Nathan Kim</span>.<br/>I'm a full-stack web developer.</h1>
+          <h1 className="intro" >Hello, I'm <span className="highlight">Nathan Kim</span>.<br/>I'm a full-stack web developer.</h1>
           <div className="card">
             <button onClick={() => setCount((count) => count + 1)}>
               I'm gonna give Nathan, {count}x kisses. 
             </button>
           </div>
 
-          <button><a href="#">See my work</a></button>
+          <button className="mb-10"><a href="#">See my work</a></button>
     
           <h2>Scroll down to See more!</h2>
         </>
